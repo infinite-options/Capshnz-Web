0cce18193310713f8602059cd35eea79b8dd85ed
pmarathay <pmarathay@infiniteoptions.com>
2025-01-06 17:41:06 -0800

    updating App.js to remove deleted files

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import Confirmation from "./components/Confirmation";
 import UserInfo from "./components/UserInfo";
 import JoinGame from "./components/JoinGame";
 import Feedback from "./components/Feedback";
-import Landing from "./components/Landing";
+
 import RoundType from "./components/RoundType";
 import ScoreType from "./components/ScoreType";
-import Waiting from "./components/Waiting";
+
 import SelectDeck from "./components/SelectDeck";
 import GooglePhotos from "./components/GooglePhotos";
-import Caption from "./components/Caption";
-import Vote from "./components/Vote";
-import ScoreBoard from "./components/ScoreBoard";
+
 import EndGame from "./components/EndGame";
 import CnnDeck from "./components/CnnDeck";
 import ApiError from "./components/ApiError";
@@ -41,48 +39,38 @@ export default function App() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   return (
-    <div className="app">
-      <ErrorContext.Provider
-        value={{ setShow, setOnRetry, setTitle, setDescription }}
-      >
-        <ApiError
-          show={show}
-          onRetry={onRetry}
-          title={title}
-          description={description}
-        />
-        <GoogleOAuthProvider clientId="336598290180-69pe1qeuqku450vnoi8v1ehhi19jhpmt.apps.googleusercontent.com">
+    <div className='app'>
+      <ErrorContext.Provider value={{ setShow, setOnRetry, setTitle, setDescription }}>
+        <ApiError show={show} onRetry={onRetry} title={title} description={description} />
+        <GoogleOAuthProvider clientId='336598290180-69pe1qeuqku450vnoi8v1ehhi19jhpmt.apps.googleusercontent.com'>
           <CookiesProvider>
             <Router>
               <Routes>
-                <Route path="/landingold" element={<Landing />} />
-                <Route path="/UserInfo" element={<UserInfo />} />
-                <Route path="/JoinGame" element={<JoinGame />} />
-                <Route path="/Feedback" element={<Feedback />} />
-                <Route path="/GameRules" element={<GameRules />} />
-                <Route path="/Confirmation" element={<Confirmation />} />
-                <Route path="/ScoreType" element={<ScoreType />} />
-                <Route path="/RoundType" element={<RoundType />} />
-                <Route path="/Waiting" element={<Waiting />} />
-                <Route path="/SelectDeck" element={<SelectDeck />} />
-                <Route path="/GooglePhotos" element={<GooglePhotos />} />
-                <Route path="/Caption" element={<Caption />} />
-                <Route path="/Vote" element={<Vote />} />
-                <Route path="/ScoreBoard" element={<ScoreBoard />} />
-                <Route path="/EndGame" element={<EndGame />} />
-                <Route path="/CnnDeck" element={<CnnDeck />} />
-                <Route path="/StartGame" element={<StartGame />} />
-                <Route path="/ChooseScoring" element={<ChooseScoring />} />
-                <Route path="/ChooseRounds" element={<ChooseRounds />} />
-                <Route path="/FinalScore" element={<FinalScore />} />
-                <Route path="/WaitingRoom" element={<WaitingRoom />} />
-                <Route path="/" element={<LandingNew />} />
-                <Route path="/VerificationOtp" element={<VerificationOtp />} />
-                <Route path="/EnterName" element={<EnterName />} />
-                <Route path="/CaptionNew" element={<CaptionNew />} />
-                <Route path="/VoteImage" element={<VoteImage />} />
-                <Route path="/ScoreboardNew" element={<ScoreboardNew />} />
-                <Route path="/MidGameWaitingRoom" element={<MidGameWaitingRoom />} />
+                <Route path='/UserInfo' element={<UserInfo />} />
+                <Route path='/JoinGame' element={<JoinGame />} />
+                <Route path='/Feedback' element={<Feedback />} />
+                <Route path='/GameRules' element={<GameRules />} />
+                <Route path='/Confirmation' element={<Confirmation />} />
+                <Route path='/ScoreType' element={<ScoreType />} />
+                <Route path='/RoundType' element={<RoundType />} />
+
+                <Route path='/SelectDeck' element={<SelectDeck />} />
+                <Route path='/GooglePhotos' element={<GooglePhotos />} />
+
+                <Route path='/EndGame' element={<EndGame />} />
+                <Route path='/CnnDeck' element={<CnnDeck />} />
+                <Route path='/StartGame' element={<StartGame />} />
+                <Route path='/ChooseScoring' element={<ChooseScoring />} />
+                <Route path='/ChooseRounds' element={<ChooseRounds />} />
+                <Route path='/FinalScore' element={<FinalScore />} />
+                <Route path='/WaitingRoom' element={<WaitingRoom />} />
+                <Route path='/' element={<LandingNew />} />
+                <Route path='/VerificationOtp' element={<VerificationOtp />} />
+                <Route path='/EnterName' element={<EnterName />} />
+                <Route path='/CaptionNew' element={<CaptionNew />} />
+                <Route path='/VoteImage' element={<VoteImage />} />
+                <Route path='/ScoreboardNew' element={<ScoreboardNew />} />
+                <Route path='/MidGameWaitingRoom' element={<MidGameWaitingRoom />} />
               </Routes>
             </Router>
           </CookiesProvider>
